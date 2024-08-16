b3c15dd5b86609c3cc2d76b10b1d2bdf7fd47632
Rafet17 <rafet.hasanov17@gmail.com>
2024-08-16 11:19:21 +0400

    About section and responsive design

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,9 @@ const mobileMenuIcon = document.querySelector(".mobile-menu-icon");
 const mobileMenu = document.querySelector(".header-menu-wrap");
 const mobileMenuClose = document.querySelector(".mobile-menu-close");
 
+
+
+
 //popup search
 document.querySelector(".search-icon").onclick = () => {
     searchBox.classList.toggle("active")
@@ -53,32 +56,47 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
-mobileMenuIcon.addEventListener('click', () =>{
+mobileMenuIcon.addEventListener('click', () => {
     mobileMenu.classList.toggle("open")
 })
 
 //*----------------ANIMATIONS----------------/
 
-function animation(){
+function animation() {
     let controller = new ScrollMagic.Controller();
     const t1 = gsap.timeline({ defaults: { ease: Expo.InOut } });
 
-    t1.fromTo(".car-holder img", 1, { x: "25rem"}, { x: 0});
-    t1.fromTo(".caption.medium", 1.1, {y:"4rem", opacity:0}, {y:0, opacity:1}, "-=1");
-    t1.fromTo(".caption.big", 1.2, {y:"5rem", opacity:0}, {y:0, opacity:1}, "-=1");
-    t1.fromTo(".caption.small", 1.3, {y:"6rem", opacity:0}, {y:0, opacity:1}, "-=1");
-    t1.fromTo(".button", 1.4, {y:"7rem", opacity:0}, {y:0, opacity:1, stagger: 0.5}, "-=1");
-    t1.fromTo(".slide-in-bottom", 1.3, {y:"25rem"}, {y:0}, "-=1");
-    t1.fromTo(".fad-in", 1.3, {opacity:0}, {opacity:1}, "-=1");
-    t1.fromTo(".slide-in-top", 1.3, {y:"-25rem"}, {y:0}, "-=1");
+    t1.fromTo(".car-holder img", 1, { x: "25rem" }, { x: 0 });
+    t1.fromTo(".caption.medium", 1.1, { y: "4rem", opacity: 0 }, { y: 0, opacity: 1 }, "-=1");
+    t1.fromTo(".caption.big", 1.2, { y: "5rem", opacity: 0 }, { y: 0, opacity: 1 }, "-=1");
+    t1.fromTo(".caption.small", 1.3, { y: "6rem", opacity: 0 }, { y: 0, opacity: 1 }, "-=1");
+    t1.fromTo(".button", 1.4, { y: "7rem", opacity: 0 }, { y: 0, opacity: 1, stagger: 0.5 }, "-=1");
+    t1.fromTo(".slide-in-bottom", 1.3, { y: "25rem" }, { y: 0 }, "-=1");
+    t1.fromTo(".fad-in", 1.3, { opacity: 0 }, { opacity: 1 }, "-=1");
+    t1.fromTo(".slide-in-top", 1.3, { y: "-25rem" }, { y: 0 }, "-=1");
+
+
+    //! About animations/
+    const t2 = gsap.timeline({ defaults: { ease: Expo.InOut } });
+
+    t2.fromTo(".about-img1", 1, { x: "-6rem", opacity: 0 }, { x: 0, opacity: 1 });
+    t2.fromTo(".about-img2", 1, { y: "5rem", opacity: 0 }, { y: 0, opacity: 1 }, "-=1");
+    t2.fromTo(".about-heading", 1, { x: "5rem", opacity: 0 }, { x: 0, opacity: 1 }, "-=1");
+    t2.fromTo(".about-info-text", 1.5, { x: "5rem", opacity: 0 }, { x: 0, opacity: 1 }, "-=1");
+
+    new ScrollMagic.Scene({
+        triggerElement: "#about",
+        triggerHook: 0.5,
+        reverse: false
+    }).setTween(t2).addTo(controller);
 }
 
 
 animation();
-    
 
- 
-   
+
+
+
 
 
 
