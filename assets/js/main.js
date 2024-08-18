ffd8f60f6ef36ed81514e2fb50438a26e82e8063
Rafet17 <rafet.hasanov17@gmail.com>
2024-08-18 13:33:26 +0400

    Home, About, Service, Booking section's mobile animations. File structure updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,13 +4,12 @@ const stickyHeader = document.querySelector(".sticky-header");
 const mobileMenuIcon = document.querySelector(".mobile-menu-icon");
 const mobileMenu = document.querySelector(".header-menu-wrap");
 const mobileMenuClose = document.querySelector(".mobile-menu-close");
-let searchIcon =document.querySelectorAll(".search-icon");
+let searchIcon = document.querySelectorAll(".search-icon");
 
 
 
 
-//popup search
-
+//*----------------SEARCH START----------------/
 
 let mainSearchIcon = document.querySelector(".main-header .search-icon");
 let stickySearchIcon = document.querySelector(".sticky-header .search-icon");
@@ -38,12 +37,11 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 const observer = new MutationObserver((mutationsList, observer) => {
-    for(let mutation of mutationsList) {
+    for (let mutation of mutationsList) {
         if (mutation.type === 'childList') {
-            // Örneğin sticky header DOM'a eklendikten sonra event listener ekleme.
+            
             if (document.querySelector(".sticky-header")) {
-                observer.disconnect(); // Gözlemlemeyi durdur.
-                // Buraya event listener'ları ekleyin.
+                observer.disconnect(); // 
             }
         }
     }
@@ -51,10 +49,12 @@ const observer = new MutationObserver((mutationsList, observer) => {
 
 observer.observe(document.body, { childList: true, subtree: true });
 
+//*----------------SEARCH END----------------/
 
 
 
-//popup sidebox
+
+//*----------------SIDEBOX START----------------/
 document.querySelector(".sidebox-icon").onclick = () => {
     sideBox.classList.toggle("active")
 }
@@ -75,6 +75,12 @@ window.onscroll = () => {
     sideBox.classList.remove("active");
 }
 
+//*----------------SIDEBOX END----------------/
+
+
+
+//*----------------STICKY HEADER START----------------/
+
 document.addEventListener('DOMContentLoaded', function () {
     const stickyHeader = document.querySelector('.sticky-header');
     const mainHeader = document.querySelector(".main-header");
@@ -85,7 +91,7 @@ document.addEventListener('DOMContentLoaded', function () {
         if (scrollTop > mainHeader.offsetHeight) {
             stickyHeader.classList.add('sticky-fixed-top');
             stickyHeader.style.transform = 'translateY(0)';
-            
+
 
         }
         else if (scrollTop < mainHeader.offsetHeight) {
@@ -97,79 +103,150 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 
+//*----------------STICKY HEADER START----------------/
 
 
 
+//*----------------MOBILE MENU HEADER START----------------/
 
 mobileMenuIcon.addEventListener('click', () => {
     mobileMenu.classList.toggle("open")
-})
+});
 
-//*----------------ANIMATIONS----------------/
+//*----------------MOBILE MENU HEADER END----------------/
+
+
+
+//*----------------ANIMATIONS START----------------/
 
 function animation() {
     let controller = new ScrollMagic.Controller();
     const t1 = gsap.timeline({ defaults: { ease: Expo.InOut } });
-
-    t1.fromTo(".car-holder img", 1, { x: "25rem" }, { x: 0 });
-    t1.fromTo(".caption.medium", 1.1, { y: "4rem", opacity: 0 }, { y: 0, opacity: 1 }, "-=1");
-    t1.fromTo(".caption.big", 1.2, { y: "5rem", opacity: 0 }, { y: 0, opacity: 1 }, "-=1");
-    t1.fromTo(".caption.small", 1.3, { y: "6rem", opacity: 0 }, { y: 0, opacity: 1 }, "-=1");
-    t1.fromTo(".button", 1.4, { y: "7rem", opacity: 0 }, { y: 0, opacity: 1, stagger: 0.5 }, "-=1");
-    t1.fromTo(".slide-in-bottom", 1.3, { y: "25rem" }, { y: 0 }, "-=1");
-    t1.fromTo(".fad-in", 1.3, { opacity: 0 }, { opacity: 1 }, "-=1");
-    t1.fromTo(".slide-in-top", 1.3, { y: "-25rem" }, { y: 0 }, "-=1");
-
-
-    //! About animations/
     const t2 = gsap.timeline({ defaults: { ease: Expo.InOut } });
+    const t3 = gsap.timeline({ defaults: { ease: Expo.InOut } });
+    const t4 = gsap.timeline({ defaults: { ease: Expo.InOut } });
 
-    t2.fromTo(".about-img1", 1, { x: "-6rem", opacity: 0 }, { x: 0, opacity: 1 });
-    t2.fromTo(".about-img2", 1, { y: "5rem", opacity: 0 }, { y: 0, opacity: 1 }, "-=1");
-    t2.fromTo(".about-heading", 1, { x: "5rem", opacity: 0 }, { x: 0, opacity: 1 }, "-=1");
-    t2.fromTo(".about-info-text", 1.5, { x: "5rem", opacity: 0 }, { x: 0, opacity: 1 }, "-=1");
+    if (window.matchMedia("(max-width: 900px)").matches) {
 
-    new ScrollMagic.Scene({
-        triggerElement: "#about",
-        triggerHook: 0.5,
-        reverse: false
-    }).setTween(t2).addTo(controller);
+        //! Home mobile animations/
+        t1.fromTo(".car-holder img", 1, { x: "25rem" }, { x: 0 });
+        t1.fromTo(".caption.medium", 1.1, { y: "4rem", opacity: 0 }, { y: 0, opacity: 1 }, "-=1");
+        t1.fromTo(".caption.big", 1.2, { y: "5rem", opacity: 0 }, { y: 0, opacity: 1 }, "-=1");
+        t1.fromTo(".caption.small", 1.3, { y: "6rem", opacity: 0 }, { y: 0, opacity: 1 }, "-=1");
+        t1.fromTo(".button", 1.4, { y: "7rem", opacity: 0 }, { y: 0, opacity: 1, stagger: 0.5 }, "-=1");
+        t1.fromTo(".slide-in-bottom", 1.3, { y: "25rem" }, { y: 0 }, "-=1");
+        t1.fromTo(".fad-in", 1.3, { opacity: 0 }, { opacity: 1 }, "-=1");
+        t1.fromTo(".slide-in-top", 1.3, { y: "-25rem" }, { y: 0 }, "-=1");
+
+
+        //! About mobile animations/
+        t2.fromTo(".about-img1", 1.5, { x: "-6rem", opacity: 0 }, { x: 0, opacity: 1 });
+        t2.fromTo(".about-img2", 1, { y: "5rem", opacity: 0 }, { y: 0, opacity: 1 });
+        t2.fromTo(".about-heading", 1, { x: "5rem", opacity: 0 }, { x: 0, opacity: 1 });
+        t2.fromTo(".about-info-text", 1.5, { x: "5rem", opacity: 0 }, { x: 0, opacity: 1 });
+
+
+        //! Service mobile animations/
+        t3.fromTo(".service-heading h4, .service-heading h2, .service-heading p", 1, { y: "8rem", opacity: 0 }, { y: 0, opacity: 1 });
+        t3.fromTo(".service-item", 1, { y: "4rem", opacity: 0 }, { y: 0, opacity: 1, stagger: 0.5 });
+
+        new ScrollMagic.Scene({
+            triggerElement: "#service",
+            triggerHook: 1,
+            reverse: false
+        }).setTween(t3).addTo(controller);
+
+    }
+    else {
+        t1.fromTo(".car-holder img", 1, { x: "25rem" }, { x: 0 });
+        t1.fromTo(".caption.medium", 1.1, { y: "4rem", opacity: 0 }, { y: 0, opacity: 1 }, "-=1");
+        t1.fromTo(".caption.big", 1.2, { y: "5rem", opacity: 0 }, { y: 0, opacity: 1 }, "-=1");
+        t1.fromTo(".caption.small", 1.3, { y: "6rem", opacity: 0 }, { y: 0, opacity: 1 }, "-=1");
+        t1.fromTo(".button", 1.4, { y: "7rem", opacity: 0 }, { y: 0, opacity: 1, stagger: 0.5 }, "-=1");
+        t1.fromTo(".slide-in-bottom", 1.3, { y: "25rem" }, { y: 0 }, "-=1");
+        t1.fromTo(".fad-in", 1.3, { opacity: 0 }, { opacity: 1 }, "-=1");
+        t1.fromTo(".slide-in-top", 1.3, { y: "-25rem" }, { y: 0 }, "-=1");
+
+
+        //! About animations/
+
+
+        t2.fromTo(".about-img1", 1, { x: "-6rem", opacity: 0 }, { x: 0, opacity: 1 });
+        t2.fromTo(".about-img2", 1, { y: "5rem", opacity: 0 }, { y: 0, opacity: 1 }, "-=1");
+        t2.fromTo(".about-heading", 1, { x: "5rem", opacity: 0 }, { x: 0, opacity: 1 }, "-=1");
+        t2.fromTo(".about-info-text", 1.5, { x: "5rem", opacity: 0 }, { x: 0, opacity: 1 }, "-=1");
+
+        new ScrollMagic.Scene({
+            triggerElement: "#about",
+            triggerHook: 0.5,
+            reverse: false
+        }).setTween(t2).addTo(controller);
+
+
+
+        //! Service animations/
+        t3.fromTo(".service-heading h4, .service-heading h2, .service-heading p", 1, { y: "4rem", opacity: 0 }, { y: 0, opacity: 1 });
+        t3.fromTo(".service-item", 1, { y: "4rem", opacity: 0 }, { y: 0, opacity: 1, stagger: 0.5 })
+
+        new ScrollMagic.Scene({
+            triggerElement: "#service",
+            triggerHook: 0.5,
+            reverse: false
+        }).setTween(t3).addTo(controller);
+
+
+
+        //! Booking animations/
+        t4.fromTo(".booking-car", 1, {x:"-8rem", opacity:0}, {x:0, opacity:1})
+
+        new ScrollMagic.Scene({
+            triggerElement: "#booking",
+            triggerHook: 0.5,
+            reverse: false
+        }).setTween(t4).addTo(controller);
+
+    }
 }
 
 
 animation();
 
+//*----------------ANIMATIONS END----------------/
 
+
+//*----------------SWIPER START----------------/
 
 let swiperCards = new Swiper('.mySwiper', {
     slidesPerView: 1,
     loop: true,
-    spaceBetween:30,
-    grabCursor:true,
-  
+    spaceBetween: 30,
+    grabCursor: true,
+
     // If we need pagination
     pagination: {
-      el: '.swiper-pagination',
-      clickable:true,
+        el: '.swiper-pagination',
+        clickable: true,
     },
-  
+
     // Navigation arrows
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
     },
-    
-    breakpoints:{
-        900:{
+
+    breakpoints: {
+        900: {
             slidesPerView: 3
         }
     }
-  });
+});
 
-  
+//*----------------SWIPER END----------------/
 
 
-  
+
+
+
 
 
 
